aab803f67e7267fe8c5f2e5fb398c63f5f64e69f
estokstad <144620217+estokstad@users.noreply.github.com>
2024-11-22 16:43:11 +0100

    setup and test-scene added

---
+++ scripts/grass_cylinder.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var ROTATIONSPEED = 1
+
+@onready var animatable_body_3d: AnimatableBody3D = $AnimatableBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	animatable_body_3d.rotation.z += delta * ROTATIONSPEED
+	
