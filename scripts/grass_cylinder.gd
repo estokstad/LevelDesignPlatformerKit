e43bb0f9c0a7208a0d8010c809a837f05285808e
estokstad <144620217+estokstad@users.noreply.github.com>
2024-11-29 18:35:16 +0100

    Blockout

--- scripts/grass_cylinder.gd
+++ scripts/grass_cylinder.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @export var ROTATIONSPEED = 1
 @onready var collision_shape_3d: CollisionShape3D = $AnimatableBody3D/CollisionShape3D
-@onready var animatable_body_3d: AnimatableBody3D = $AnimatableBody3D
+@onready var grass_cylinder: AnimatableBody3D = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +12,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	animatable_body_3d.rotation.z += delta * ROTATIONSPEED
+	grass_cylinder.rotation.z += delta * ROTATIONSPEED
--- scripts/spawn_scene.gd
+++ scripts/spawn_scene.gd
@@ -3,30 +3,22 @@ extends Node3D
 
 
 @onready var spawn_scene: Node3D = $"."
-
 @export var is_on_from_start : bool = true
-@export var look_at_object_when_activated : bool
-@onready var scene_position : Transform3D
-
-var scene_to_spawn = preload("res://objects/Ed-Objects/scene_to_spawn.tscn").instantiate()
+@export var scene_to_spawn: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_scene.transform = scene_position
-	
-	scene_to_spawn.transform = scene_position
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not Engine.is_editor_hint():
 		if is_on_from_start:
-			scene_to_spawn = load("res://objects/Ed-Objects/scene_to_spawn.tscn")
+			scene_to_spawn.visible = true
+		else:
+			scene_to_spawn.visible = false
 
 			
 		
 
 func receive_input(on : bool) -> void:
 	is_on_from_start = on
-	
-	if on and look_at_object_when_activated:
-		get_tree().call_group("View", "look_at_target",spawn_scene)
